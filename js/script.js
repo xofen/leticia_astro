9db5f1cbf2bce5c61484243e52df0dde2d055930
Xofen <pedromirilli@gmail.com>
2024-03-06 15:36:06 -0300

    ALPHA

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function myFunction() {
   var x = document.getElementById("menu");
   var y = document.getElementById("burger");
-  if (x.style.display === "block") {
+  var z = document.getElementById("main");
+  if (x.style.display === "flex") {
     x.style.display = "none";
     y.innerHTML = '<i class="fa-solid fa-plus"></i>';
+    z.style.display = "block";
   } else {
-    x.style.display = "block";
-    x.style.animation = "droplow 1.3s";
+    x.style.display = "flex";
     y.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    z.style.display = "none";
   }
 }
