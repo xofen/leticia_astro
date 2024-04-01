a8cf19a60a0a7711cdbbbc1dea60b7c7bd6dd695
Xofen <pedromirilli@gmail.com>
2024-04-01 11:31:44 -0300

    mobile_com_alt

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ function myFunction() {
   var x = document.getElementById("menu");
   var y = document.getElementById("burger");
   var z = document.getElementById("main");
+  var f = document.getElementById("footer");
   if (x.style.display === "flex") {
     x.style.display = "none";
     y.innerHTML = '<i class="fa-solid fa-plus"></i>';
     z.style.display = "block";
+    f.style.display = "flex";
   } else {
     x.style.display = "flex";
     y.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     z.style.display = "none";
+    f.style.display = "none";
   }
 }
